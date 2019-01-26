69a504966eee64196d0bce61c0016e4b74887dae
Raymun Kohli <raymun1@hotmail.co.uk>
2019-01-26 00:35:06 +0000

    support for basic machine

--- convertToJSON.py
+++ convertToJSON.py
@@ -1,14 +1,84 @@
-import sys, getopt
+import sys, getopt,json
+import xml.etree.ElementTree as ET
+#https://docs.python.org/3/library/xml.etree.elementtree.html
 from optparse import *
+ESLI = "{http://www.w3.org/2001/XMLSchema-instance}"
 
 def main():
-    #define the arguments
-    xmlFile = ""
-    outputDirectory = ""
+    # match arguments to variables
     args = parser()
-    xmlFile = args[0]
-    outputDirectory = args[1]
-    print(xmlFile, outputDirectory)
+    xmlfile = args[0]
+    outputdirectory = args[1]
+    deconstruct_tree(ET.parse(xmlfile))
+
+
+def deconstruct_tree(tree):
+    for child in tree.getroot():
+        if child.tag == "machine":
+            if child.get(ESLI+"type") == "cw:Link":
+                a = 1+ 1
+            else:
+                processMachine(child)
+        elif child.tag == "network":
+            a = 1+1
+
+
+def processMachine(machine):
+    states = []
+    transitions = {}
+    # get states
+    initial = machine.get("initial").split(".")[-1]
+    #  get initial state
+    for child in machine:
+        #check if child is a state
+        if child.tag == "state":
+            states.append(child.get("name"))
+        # check if child is a transition
+        if child.tag == "transition":
+            fromid = states[int(child.get("from").split(".")[-1])]
+            toid = states[int((child.get("to").split(".")[-1]))]
+            #check if transition is probabilistic  or not
+            if child.get(ESLI+"type") == "cw:probabilistic":
+                transition = {
+                    toid: [
+                        {
+                            "type" : "probabilistic",
+                            "distribution": child.get("distribution"),
+                            "parameter": child.get("parameter")
+                            }
+                        ]
+                    }
+            else:
+                transition = {
+                    states[toid]: [
+                        {
+                            "type": "deterministic",
+                            "parameter": child.get("parameter")
+                        }
+                    ]
+                }
+
+            if fromid in transitions:
+                transitions[fromid] = transitions[fromid] + transition
+                print("old")
+            else:
+                print("new")
+                transitions[fromid] = transition
+
+
+    # get initial state
+    data = {
+        "name": machine.get("name"),
+        "type": machine.get("type"),
+        "structure": {
+            "states": states,
+            "initial": states[int(initial)],
+            "transitions": transitions
+        }
+    }
+    print(json.dumps(data))
+
+
 
 def parser():
     parser = OptionParser(usage="usage: convertToJSON.py <XMLFILE> <OUTPUT_DIRECTORY>")
---
+++ doublem.cw
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="ASCII"?>
+<cw:Model xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cw="http://www.example.org/cw" name="length">
+  <machine xsi:type="cw:Link" target="//@network.1/@represented" source="//@network.0/@represented">
+    <property name="kv" value="state-machine" type="string"/>
+    <property name="x" type="number"/>
+    <property name="max" type="number"/>
+    <property name="overloaded" type="boolean"/>
+    <property name="connected" type="boolean"/>
+    <property name="length" type="number"/>
+  </machine>
+  <machine initial="//@machine.1/@state.0" name="Battery" type="state-machine">
+    <state name="ok"/>
+    <state name="fail"/>
+    <transition xsi:type="cw:probabilistic" to="//@machine.1/@state.1" from="//@machine.1/@state.0" parameter="0.1" distribution="exponential"/>
+    <transition xsi:type="cw:probabilistic" to="//@machine.1/@state.0" from="//@machine.1/@state.1" parameter="20.0"/>
+  </machine>
+  <network>
+    <machine name="mamamia" parent="//@network.0"/>
+    <represented initial="//@network.0/@represented/@state.0" name="123">
+      <state name="ok"/>
+      <state name="fail"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.0/@represented/@state.0" from="//@network.0/@represented/@state.1" parameter="0.1" distribution="exponential"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.0/@represented/@state.1" from="//@network.0/@represented/@state.0" parameter="20.0" distribution="exponential"/>
+      <property name="Load"/>
+    </represented>
+    <network super="//@network.0">
+      <represented name="123123" type="network-machine" parent="//@network.0"/>
+    </network>
+  </network>
+  <network>
+    <represented initial="//@network.1/@represented/@state.0" name="333">
+      <state name="ok"/>
+      <state name="fail"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.1/@represented/@state.0" from="//@network.1/@represented/@state.1" parameter="0.1" distribution="exponential"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.1/@represented/@state.1" from="//@network.1/@represented/@state.0" parameter="20.0" distribution="exponential"/>
+      <property name="Load" value="123" type="Number"/>
+    </represented>
+    <network super="//@network.1">
+      <represented name="Relay" parent="//@network.1">
+        <property/>
+      </represented>
+    </network>
+  </network>
+</cw:Model>
