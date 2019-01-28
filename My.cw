291b68c770712fc04d3c07af0df830423c5d6c4b
Raymun Kohli <raymun1@hotmail.co.uk>
2019-01-28 00:57:37 +0000

    fixed suffixes in networks and machines

--- My.cw
+++ My.cw
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="ASCII"?>
 <cw:Model xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:cw="http://www.example.org/cw" name="length">
-  <machine initial="//@machine.0/@state.0" name="Battery" type="state-machine">
+  <machine initial="//@machine.0/@state.0" name="Batteryy" type="state-machine">
     <state name="ok"/>
     <state name="fail"/>
     <transition xsi:type="cw:probabilistic" to="//@machine.0/@state.1" from="//@machine.0/@state.0" parameter="13.3" distribution="exponential"/>
     <transition xsi:type="cw:probabilistic" to="//@machine.0/@state.0" from="//@machine.0/@state.1" parameter="333.0" distribution="exponential"/>
-    <property name="Capasity" value="103" type="Number"/>
+    <property name="Capisity" value="103" type="Number"/>
   </machine>
   <machine xsi:type="cw:Link" name="sub123-sub101-0" type="state-machine" target="//@network.1/@represented" source="//@network.0/@represented">
     <property name="kv" value="state-machine" type="string"/>
@@ -16,6 +16,13 @@
     <property name="length" type="number"/>
   </machine>
   <network>
+    <machine initial="//@network.0/@machine.0/@state.0" name="Battery" type="state-machine">
+      <state name="ok"/>
+      <state name="fail"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.0/@machine.0/@state.1" from="//@network.0/@machine.0/@state.0" parameter="13.3" distribution="exponential"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.0/@machine.0/@state.0" from="//@network.0/@machine.0/@state.1" parameter="333.0" distribution="exponential"/>
+      <property name="Capisity" value="103" type="Number"/>
+    </machine>
     <represented initial="//@network.0/@represented/@state.0" name="sub123" type="state-machine">
       <state name="ok"/>
       <state name="fail"/>
@@ -24,16 +31,39 @@
       <property name="Load" value="123" type="Number"/>
     </represented>
     <network super="//@network.0">
+      <machine initial="//@network.0/@network.0/@machine.0/@state.0" name="Battery" type="state-machine">
+        <state name="ok"/>
+        <state name="fail"/>
+        <transition xsi:type="cw:probabilistic" to="//@network.0/@network.0/@machine.0/@state.1" from="//@network.0/@network.0/@machine.0/@state.0" parameter="13.3" distribution="exponential"/>
+        <transition xsi:type="cw:probabilistic" to="//@network.0/@network.0/@machine.0/@state.0" from="//@network.0/@network.0/@machine.0/@state.1" parameter="333.0" distribution="exponential"/>
+        <property name="Capisity" value="103" type="Number"/>
+      </machine>
       <represented name="New Network" type="network-machine" parent="//@network.0"/>
     </network>
   </network>
   <network>
+    <machine initial="//@network.1/@machine.0/@state.0" name="Battery" type="state-machine">
+      <state name="ok"/>
+      <state name="fail"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.1/@machine.0/@state.1" from="//@network.1/@machine.0/@state.0" parameter="13.3" distribution="exponential"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.1/@machine.0/@state.0" from="//@network.1/@machine.0/@state.1" parameter="333.0" distribution="exponential"/>
+      <property name="Capisity" value="103" type="Number"/>
+    </machine>
     <represented initial="//@network.1/@represented/@state.0" name="sub101" type="state-machine">
       <state name="ok"/>
       <state name="fail"/>
       <transition xsi:type="cw:probabilistic" to="//@network.1/@represented/@state.0" from="//@network.1/@represented/@state.1" parameter="0.1" distribution="exponential"/>
       <transition xsi:type="cw:probabilistic" to="//@network.1/@represented/@state.1" from="//@network.1/@represented/@state.0" parameter="20.0" distribution="exponential"/>
       <property name="Load" value="333" type="Number"/>
+    </represented>
+  </network>
+  <network>
+    <represented initial="//@network.2/@represented/@state.0" name="sub101" type="state-machine">
+      <state name="ok"/>
+      <state name="fail"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.2/@represented/@state.0" from="//@network.2/@represented/@state.1" parameter="0.1" distribution="exponential"/>
+      <transition xsi:type="cw:probabilistic" to="//@network.2/@represented/@state.1" from="//@network.2/@represented/@state.0" parameter="20.0" distribution="exponential"/>
+      <property name="Load" value="222" type="Number"/>
     </represented>
   </network>
 </cw:Model>
--- convertToJSON.py
+++ convertToJSON.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 from optparse import *
 ESLI = "{http://www.w3.org/2001/XMLSchema-instance}"
 
+
 def main():
     # match arguments to variables
     args = parser()
@@ -13,54 +14,85 @@ def main():
         os.mkdir(outputdirectory+"/machines/")
     if not os.path.exists(outputdirectory+"/network/"):
         os.mkdir(outputdirectory + "/network/")
-    deconstruct_tree(ET.parse(xmlfile),outputdirectory)
+    deconstruct_tree(ET.parse(xmlfile), outputdirectory)
 
+def calculate_suffix(name,array):
+    i = 0
+    count = 0
+    for x in  array:
+        if name+'_'+str(count) == array[i]:
+            count +=1
+        i += 1
+    return count
 
 def deconstruct_tree(tree, outputdirectory):
     machines = []
     networks = []
+    file = open(outputdirectory + '/' + tree.getroot().get("name")+".json", 'w+')
 
     for child in tree.getroot():
         if child.get("type") == "state-machine":
             if child.get(ESLI+"type") == "cw:Link":
                 a = 1 + 1
             else:
-                i = 0
-                for x in machines:
-                    if child.get("name") == machines[i]:
-                        i = i + 1
-                print("machine :" + child.get("name"))
-                machines.append(process_machine(child, outputdirectory, i))
-        elif child.get("type") == "network-machine":
-            print("network M :")
-            process_networks(child, outputdirectory, machines, networks)
+                u = process_machine(child, outputdirectory, calculate_suffix(child.get("name"),machines), machines)
+                print(machines)
+                #instance the machine ** to do
         elif child.tag == "network":
-            print("network N :")
             process_networks(child, outputdirectory, machines, networks)
         else:
             sys.exit("unsupported type on:" + child.tag)
+    m = []
+    for mach in machines:
+        d = {
+            "include": "machines/"+str(mach)
+        }
+        m.append(d)
+
+    n = []
+    for net in networks:
+        d = {
+            "include": "networks/"+str(net)
+        }
+        n.append(d)
+
+    info = {
+        "description": "Substations",
+        "machines": m,
+        "networks": n
+    }
+    file.write(json.dumps(info))
 
 
 def process_networks(network, outputdirectory, machines, networks):
-    networks.append(network)
     localmachine = []
+    a = "null"
     for child in network:
         if child.get("type") == "state-machine":
-            i = 0
-            for x in machines:
-                if child.get("name") == machines[i]:
-                    i = i + 1
-            machines.append(process_machine(child, outputdirectory, i))
-            #localmachine.append()
-        if child.get("type") == "network-machine":
-            i = 0
-            for x in networks:
-                if child.get("name") == networks[i]:
-                    i = i + 1
-            machines.append(process_represents(child, outputdirectory, i, machines))
-    return 123
+            m = process_machine(child, outputdirectory, calculate_suffix(child.get("name"),machines), machines)
+            a = m[2]
+            if child.tag != 'represented':
+                localmachine.append(m[0])
+            else:
+                abc=1
 
-def process_represents(machine, outputdirectory, suffix, machines):
+        if child.get("type") == "network-machine":
+            a = process_netmachine(child, outputdirectory, calculate_suffix(child.get("name"),machines), machines)
+            machines.append(a)
+        elif child.tag == "network":
+            process_networks(child, outputdirectory, machines, networks)
+
+    net = {
+        "name": a,
+        "machines": localmachine
+    }
+
+    file = open(outputdirectory+"/network/"+a+".json", 'w+')
+    file.write(str(json.dumps(net)))
+
+
+
+def process_netmachine(machine, outputdirectory, suffix, machines):
     suffix = str(suffix)
     property = {}
     for child in machine:
@@ -74,19 +106,21 @@ def process_represents(machine, outputdirectory, suffix, machines):
             property[child.get("name")] = prop
 
     data = {
-        "name": machine.get("name"),
+        "name": machine.get("name")+'_'+suffix,
         "type": "network-machine",
         "properties": property
     }
     file = open(outputdirectory+"/machines/"+machine.get("name")+"_"+suffix+".json", 'w+')
     file.write(str(json.dumps(data)))
+    return machine.get("name")+'_'+suffix
 
 
-def process_machine(machine, outputdirectory, suffix):
+def process_machine(machine, outputdirectory, suffix, machines):
     suffix = str(suffix)
     states = []
     transitions = {}
     properties = {}
+    init = {}
     # get states
     for child in machine:
         #check if child is a state
@@ -118,10 +152,14 @@ def process_machine(machine, outputdirectory, suffix):
                 transitions[fromid].append(transition)
         if child.tag == "property":
             prop = {
+                "name": child.get("name"),
                 "type": child.get("type"),
                 "required": True,
             }
+            init[child.get("name")] = child.get("value")
             properties[child.get("name")] = prop
+
+
     data = {
         "name": machine.get("name")+"_"+suffix,
         "type": "state-machine",
@@ -132,8 +170,14 @@ def process_machine(machine, outputdirectory, suffix):
             "transitions": transitions
         }
     }
+    names = {
+        "name":machine.get("name"),
+        "machine":machine.get("name")+"_"+suffix
+    }
     file = open(outputdirectory+"/machines/"+machine.get("name")+"_"+suffix+".json", 'w+')
     file.write(str(json.dumps(data)))
+    machines.append(machine.get("name")+"_"+suffix)
+    return names, init, machine.get("name")+"_"+suffix
 
 
 def parser():
